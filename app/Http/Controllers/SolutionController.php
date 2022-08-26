a1fcb9a11355c245957488fb936fe51ba1088407
shahriar201 <shahriarbrine201@gmail.com>
2022-08-26 22:58:46 +0600

    first commit

---
+++ app/Http/Controllers/SolutionController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SolutionController extends Controller
+{
+    public function solutionOne() {
+        dd('Ok');
+    }
+}
