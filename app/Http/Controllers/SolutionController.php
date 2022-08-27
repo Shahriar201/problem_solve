d8cbf76851ea192e893831644221a01054b61253
shahriar201 <shahriarbrine201@gmail.com>
2022-08-27 11:09:54 +0600

    solution 2 on hold

--- app/Http/Controllers/SolutionController.php
+++ app/Http/Controllers/SolutionController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use DB;
+use IPset\Ipset;
 
 class SolutionController extends Controller
 {
@@ -18,6 +21,27 @@ class SolutionController extends Controller
     }
 
     public function solutionThree() {
-        dd('solution 3');
+    //     $ipaddress = '192.168.0.1';
+    //    if (isset($_SERVER['HTTP_CLIENT_IP']))
+    //        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    //    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+    //        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    //    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+    //        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    //    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+    //        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    //    else if(isset($_SERVER['HTTP_FORWARDED']))
+    //        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    //    else if(isset($_SERVER['REMOTE_ADDR']))
+    //        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    //    else
+    //        $ipaddress = 'UNKNOWN';
+    //    return $ipaddress;
+
+        Validator::extend('ip_range',function ($attribute, $value, $parameters){
+            $ipset = new IPSet(array('192.168.1.0/24'));
+            return ($ipset->match($value));
+
+        });
     }
 }
